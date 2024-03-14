5e793c9ffadb60cee066cc70dcd219ce21abeeeb
Rúben Dias <aluno24705@ipt.pt>
2024-03-14 15:02:24 +0000

    showing roots

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Eq2Grau.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+using static System.Math;
 
 namespace Eq2Grau.Controllers
 {
@@ -13,8 +15,45 @@ namespace Eq2Grau.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(String A, String B, String C)
         {
+            if (string.IsNullOrEmpty(A) || string.IsNullOrEmpty(B) || string.IsNullOrEmpty(C))
+            {
+                //tentar com viewData
+                //criar secções dentro do bag para guardar conteudo
+
+                ViewBag.Mensagem ="valor nulo";
+                return View();
+            }
+            float a;
+            float b;
+            float c;
+            if (!float.TryParse(A, out a)) {
+                ViewBag.Mensagem = "A is not a number";
+            }
+            if(!float.TryParse(B, out b)) {
+                ViewBag.Mensagem = "B is not a number";
+            }
+            if(!float.TryParse(C, out c)) {
+                ViewBag.Mensagem = "C is not a number";
+            }
+            /* Algorithm
+             * Primeiro a fazer é validar inputs, apenas são aceites numero e não texto
+             * Se tiver tudo bem fazemos o algortimo e enviamos a solução, se não, send error message
+             * if A=/=0 ok, else not ok !
+             * Determinar x1 e x2, são as raízes
+             */
+
+            if (a != 0) { 
+            double rootPlus = ((b * -1) + Math.Sqrt((b * b) - 4 * a * c)) / 2 * a;
+            double rootMinus = ((b * -1) - Math.Sqrt((b * b) - 4 * a * c)) / 2 * a;
+            
+            ViewBag.x1 = rootPlus;
+            ViewBag.x2 = rootMinus;
+                
+            } else {
+                ViewBag.Mensagem="a cannot be Zero";
+            }
             return View();
         }
 
